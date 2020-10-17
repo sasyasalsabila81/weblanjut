389006d53f279420d9f03f6b5036b07e2f9f4026
sasyasalsabila81 <sasyasalsabila81@gmail.com>
2020-10-17 20:24:47 +0700

    PrakWeb

---
+++ mahasiswa/index.php
@@ -0,0 +1,15 @@
+
+    <h1>Tabel Mahasiswa</h1>
+    <table>
+        <tr>
+            <td>
+            Nama
+            </td>
+            <td>NPM</td>
+        </tr>
+        <tr>
+             <td><?= $nama ?></td>
+
+             <td><?= $npm ?></td>
+         </tr>
+    </table>
